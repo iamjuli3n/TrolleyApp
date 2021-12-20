2d3bf2c55d4612bb391b8aad5f159d9ef8641276
iamjuli3n <93400859+iamjuli3n@users.noreply.github.com>
2021-12-19 20:22:54 -0500

    lets get it

--- pages/login.js
+++ pages/login.js
@@ -1,8 +1,9 @@
+/*
 import React, { useEffect } from "react";
 import tw from "tailwind-styled-components";
 import { useRouter } from "next/router";
 import { signInWithPopup, onAuthStateChanged } from "firebase/auth";
-//import { auth, provider } from "../firebase";
+import { auth, provider } from "../firebase";
 
 const Login = () => {
     const router = useRouter();
